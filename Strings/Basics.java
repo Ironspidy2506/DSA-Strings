83058e2e5f97bf97223556002910f881c0859164
Priyanshu Sahu <priyanshusahu085@gmail.com>
2024-11-11 23:19:26 +0530

    first commit

---
+++ Strings/Basics.java
@@ -0,0 +1,12 @@
+package Strings;
+
+public class Basics {
+    public static void main(String[] args) {
+        // String is not a data type it's a class
+        // String has two properties - String Pool, Immutability
+        // String Pool
+        // If we have to create mutliple strings with same name then instead of creating multiple objects of it, code only creates a pool of it
+        String name = "Priyanshu Sahu";
+        System.out.println(name);
+    }
+}
